2ec88c13e5a76fc21e19619bb06e7438f60ae47f
Tushar Dey <tushar24dey@gmail.com>
2024-01-30 13:20:24 +0530

    done

---
+++ index.js
@@ -0,0 +1,212 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+
+let dropdowns = document.getElementsByTagName('select');
+let button = document.querySelector('.btnDiv button');
+
+for (let select of dropdowns) {
+    for (let country in countryList) {
+        let optionEle = document.createElement('option');
+        optionEle.innerText = country;
+        optionEle.value = country;
+        select.appendChild(optionEle);
+        if (country === 'USD' && select.id === 'fromSelect') {
+            optionEle.selected = true;
+        }
+        if (country === 'INR' && select.id === 'toSelect') {
+            optionEle.selected = true;
+        }
+    }
+    select.addEventListener('change', (event) => {
+        updateFLag(event.target);
+    })
+}
+const updateFLag = (element) => {
+    let country = element.value;
+    let value = countryList[country];
+    let url = `https://flagsapi.com/${value}/shiny/64.png`;
+    let img = element.parentElement.parentElement.querySelector('img');
+    img.src = url;
+}
+
+button.addEventListener('click', async(event) => {
+    event.preventDefault();
+    let input = document.querySelector('.amount input');
+    let value = input.value;
+    let resultTag = document.querySelector('.result h3');
+    if (value === '') {
+        value = 1;
+    }
+    let selectedValue1 = '';
+    let selectedValue2 = '';
+    for (let select of dropdowns) {
+        if (select.name === 'fromSelect') {
+            selectedValue1 = select.value;
+        } else {
+            selectedValue2 = select.value;
+        }
+    }
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${selectedValue1.toLowerCase()}/${selectedValue2.toLowerCase()}.json`;
+    let currencyValue = await fetch(url);
+    let result = await currencyValue.json();
+    resultTag.textContent = value + ' ' + selectedValue1 + ' = ' + (result[selectedValue2.toLowerCase()] * value) + ' ' + selectedValue2;
+});
